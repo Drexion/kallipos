111025a1012c501a4e473809dcc27f22b1feec5d
Drexion <83861937+Drexion@users.noreply.github.com>
2022-11-17 10:15:51 +0200

    Αλλαγή του src

--- contribution.lua
+++ contribution.lua
@@ -8,7 +8,7 @@ function Image(img)
         f:close()
         local figid = string.sub(fn,1,string.len(fn)-3)
         local src = stringify(doc.meta.image_url) or "src has not been set"
-        src = ".." .. src
+        src = "." .. src
         local contribution = stringify(doc.meta.contribution) 
         return pandoc.Image(contribution,src,nil,"fig:" .. figid)
       end
